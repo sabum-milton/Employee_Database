5d928a0ae302b2d84d5f0059656347c0a83db86b
Sabum Milton Mbah <sabummilton14@gmail.com>
2026-07-14 11:22:21 +0100

    intial commit

---
+++ employee.py
@@ -0,0 +1,29 @@
+# using dictionaries for storing employee data
+employee = {
+  101: {
+       "name":"Sabum",
+        "age": "30", 
+        "department": "IT"
+        },
+  102: {
+       "name":"John",
+        "age": "25", 
+        "department": "HR"
+        },
+  103: {
+    "name":"Alice",
+    "age":"28",
+    "department":"Finance"
+  },
+  104: {
+    "name":"Bob",
+    "age":"35", 
+    "department":"Marketing"
+}
+}
+# How to output the name, age and department of all the employees using a for loop
+for emp_id,emp_data in employee.items():
+      print(f"Employee ID:{emp_id},Name:{emp_data['name']},Age:{emp_data['age']},Department:{emp_data['department']}")
+      
+print()
+print(employee[104]["department"])
